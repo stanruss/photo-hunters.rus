0d47ff9611e7b3b5d39df98f62143079070944e4
Stanruss <stan19781@gmail.com>
2018-07-24 18:31:15 +0300

    7

--- catalog/controller/checkout/guest.php
+++ catalog/controller/checkout/guest.php
@@ -198,9 +198,9 @@ class ControllerCheckoutGuest extends Controller {
 				$json['error']['country'] = $this->language->get('error_country');
 			}
 
-			if (!isset($this->request->post['zone_id']) || $this->request->post['zone_id'] == '' || !is_numeric($this->request->post['zone_id'])) {
-				$json['error']['zone'] = $this->language->get('error_zone');
-			}
+			// if (!isset($this->request->post['zone_id']) || $this->request->post['zone_id'] == '' || !is_numeric($this->request->post['zone_id'])) {
+			// 	$json['error']['zone'] = $this->language->get('error_zone');
+			// }
 
 			// Customer Group
 			if (isset($this->request->post['customer_group_id']) && is_array($this->config->get('config_customer_group_display')) && in_array($this->request->post['customer_group_id'], $this->config->get('config_customer_group_display'))) {
