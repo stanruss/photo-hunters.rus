7e5531e1fcafec9ac0b95163122f360aba54c565
Stanruss <stan19781@gmail.com>
2018-07-25 07:53:17 +0300

    8

--- catalog/controller/checkout/guest.php
+++ catalog/controller/checkout/guest.php
@@ -194,9 +194,9 @@ class ControllerCheckoutGuest extends Controller {
 				$json['error']['postcode'] = $this->language->get('error_postcode');
 			}
 
-			if ($this->request->post['country_id'] == '') {
-				$json['error']['country'] = $this->language->get('error_country');
-			}
+			// if ($this->request->post['country_id'] == '') {
+			// 	$json['error']['country'] = $this->language->get('error_country');
+			// }
 
 			// if (!isset($this->request->post['zone_id']) || $this->request->post['zone_id'] == '' || !is_numeric($this->request->post['zone_id'])) {
 			// 	$json['error']['zone'] = $this->language->get('error_zone');
